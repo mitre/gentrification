5bda58793970be6ba639fe192d70f6658e18da28
Karen Jiang <kjiang@mitre.org>
2022-03-24 12:08:05 -0700

    Add TO DO instructions for AJ
    Former-commit-id: 2153af6af46f20220a7de5861a48795d19dfa923

--- scripts/geo.R
+++ scripts/geo.R
@@ -17,4 +17,20 @@ if (!file.exists("outputs/geo_tracts.RDS")) {
   saveRDS(tracts, "outputs/geo_tracts.RDS")
 }
 
+## TO DO For AJ ----
 
+# Goal: Return 2 .RDS files:
+# File 1 (holc_geo.RDS): Maintains the original holc file structure, lists the name of the place next to each tract
+# File 2 (holc_place.RDS): Each row is a city + state in the holc file, with the combined places geometry of intersected places. Should be 202 rows.
+
+
+
+places <- readRDS("outputs/geo_places.RDS") 
+tracts <- readRDS("outputs/geo_tracts.RDS")
+holc <- read.csv("raw data/All_Population_Weighted_HOLC_Grades_pt2_Threshold.csv")
+
+
+
+# Another task would be create an .Rmd file in the notebooks folder with an descriptive stats on this data.
+# Specifically, I'd be interested in seeing how many places intersect with the original holc boundaries. 
+# Also interested in seeing how much bigger these combined geographies are compared to the original holc boundaries.
