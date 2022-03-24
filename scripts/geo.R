dfc8eb908f450b4ff368e6b7b7214e95071bdbf8
Karen Jiang <kjiang@mitre.org>
2022-03-24 11:30:51 -0700

    Comment and clean geo.R script
    Former-commit-id: 0a0f9531fcbe35a2e1eb7fe107ed8aa520f50499

--- scripts/geo.R
+++ scripts/geo.R
@@ -1,11 +1,13 @@
 library(tigris)
 library(sp)
 
+# Downloads from tigris all the census place shapefiles if file doesn't already exist in outputs folder
 if (!file.exists("outputs/geo_places.RDS")) {
   places <- places(state = state.abb, cb = TRUE)
   saveRDS(places, "outputs/geo_places.RDS")
 }
 
+# Downloads from tigris all the tract boundaries if file doesn't already exist in outputs folder
 if (!file.exists("outputs/geo_tracts.RDS")) {
   tracts <- NULL
   for(state in state.abb){
@@ -15,6 +17,4 @@ if (!file.exists("outputs/geo_tracts.RDS")) {
   saveRDS(tracts, "outputs/geo_tracts.RDS")
 }
 
-places <- readRDS("outputs/geo_places.RDS")
-tracts <- readRDS("outputs/geo_tracts.RDS")
 
