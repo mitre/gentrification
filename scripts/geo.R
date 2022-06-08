2a39c6655a90c3ac65f9560c4c3209b0debf5b53
MITRE\ALIBERATORE <aliberatore@mitre.org>
2022-06-08 15:52:30 -0400

    Added additional documentation for geo.R
    Former-commit-id: 141d42f6710c4146de65f1d5987c857fd4fa1a46

--- scripts/geo.R
+++ scripts/geo.R
@@ -9,20 +9,32 @@ library(gridExtra)
 census_api_key("ff942edcde192676c9bf58546ca77f7e97b58a46")
 
 
+### geo_places.RDS
 # Downloads from tigris all the census place shapefiles if file doesn't already exist in outputs folder
 if (!file.exists("outputs/geo_places.RDS")) {
+  # Pulls place data from tigris
   places <- places(state = state.abb, cb = TRUE)
+  
+  # Saves to outputs
   saveRDS(places, "outputs/geo_places.RDS")
 }
 
 
+### geo_tracts.RDS
 # Downloads from tigris all the tract boundaries if file doesn't already exist in outputs folder
 if (!file.exists("outputs/geo_tracts.RDS")) {
+  # Initializes tracts
   tracts <- NULL
+  
   for(state in state.abb){
+    # Pulls tract data for a state
     state_tract <- tracts(state = state, cb = TRUE)
+    
+    # Adds tract data to other states of data
     tracts <- rbind(tracts, state_tract)
   }
+  
+  # Saves to outputs
   saveRDS(tracts, "outputs/geo_tracts.RDS")
 }
 
@@ -41,6 +53,7 @@ holc <- read.csv("raw data/All_Population_Weighted_HOLC_Grades_pt2_Threshold.csv
 
 
 ### AVOID RUNNING, TAKES ~6hrs
+### final_places_tracts.RDS
 # Creates dataframe with a tract's GEOID, state, place, and geometry.
 # N.B. tracts can be split by the place borders in which they are contained.
 if (!file.exists("outputs/final_places_tracts.RDS")) {
@@ -70,36 +83,44 @@ if (!file.exists("outputs/final_places_tracts.RDS")) {
     # Pulls the geometries for all tracts cropped by places
     state_place_tract <- st_intersection(state_tract, state_place)
   
+    # Adds state data to data for other states
     places_tracts <- rbind(places_tracts, state_place_tract)
   }
   
+  # Drops segmented geometry and adds tract geometry
   final_places_tracts <- places_tracts %>%
     st_drop_geometry() %>%
     left_join(tracts %>% select(GEOID, geometry), by = "GEOID")
   
+  # Saves to outputs
   saveRDS(final_places_tracts, "outputs/final_places_tracts.RDS")
 }
 
 
+### all_acs_pops.RDS
 # Creates dataframe with a tract's GEOID and population estimate for 2019
 if (!file.exists("outputs/all_acs_pops.RDS")) {
+  # Pulls tracts grouped by place
   final_places_tracts <- readRDS("outputs/final_places_tracts.RDS")
   
+  # Pulls unique states for iteration
   place_statefp <- unique(final_places_tracts %>%
                             pull(STATEFP))
   
   all_acs_pops <- NULL
-  
   for (i in 1:length(place_statefp)){
-    # Pulls city and state
+    # Pulls state
     state <- place_statefp[i]
     
+    # Pulls population data for tracts in 2019 for state with GEOID and population
     acs_populations <- tidycensus::get_acs(geography = "tract", variables = "B01003_001", year = 2019, state = state, geometry = FALSE) %>%
       select(GEOID, estimate)
     
+    # Adds data to dataframe for other states
     all_acs_pops <- rbind(all_acs_pops, acs_populations)
   }
   
+  # Saves to outputs
   saveRDS(all_acs_pops, "outputs/all_acs_pops.RDS")
 }
 
@@ -111,8 +132,10 @@ state_lookup <- states() %>%
   select(STATEFP, STUSPS)
 
 
+### holc_geo.RDS
 # Creates dataframe of tracts with their holc grade, assigned city, and assigned place
 if (!file.exists("outputs/holc_geo.RDS")) {
+  # Pulls tracts grouped by place
   places_tracts <- readRDS("outputs/final_places_tracts.RDS")
   
   # Tracts in a place with abbreviation instead of GEOID
@@ -127,12 +150,15 @@ if (!file.exists("outputs/holc_geo.RDS")) {
     left_join(places_tracts_table, by = "GEOID") %>%
     select(-c(geometry))
   
+  # Saves to outputs
   saveRDS(holc_geo, "outputs/holc_geo.RDS")
 }
 
 
+### holc_place.RDS
 # Each row is a city + state in the holc file, with the combined places geometry of intersected places.
 if (!file.exists("outputs/holc_place.RDS")) {
+  # Pulls tract data with city, place, and holc grade
   holc_geo <- loadRDS("outputs/holc_geo.RDS")
   
   # Pulls cities and states; 202 total
@@ -177,29 +203,35 @@ if (!file.exists("outputs/holc_place.RDS")) {
     temp_df <- data.frame(holc_city, holc_state, city_place_geo)
     colnames(temp_df) <- c("CITY", "STUSPS.CITY", "geometry")
   
+    # Adds row for specific city as sf
     holc_place = rbind(holc_place, st_as_sf(temp_df))
   }
   
+  # Saves to outputs
   saveRDS(holc_place, "outputs/holc_place.RDS")
 }
 
 
+### places_pop_gt_50k.RDS
 # places with populations of greater than 50k
 if (!file.exists("outputs/places_pop_gt_50k.RDS")) {
+  # Pulls tracts grouped by place
   places_tracts <- loadRDS("outputs/final_places_tracts.RDS")
 
+  # Pulls states
   place_statefp <- unique(places_tracts %>%
                            pull(STATEFP))
   
   population_table <- NULL
-  
   for (i in 1:length(place_statefp)) {
-    # Pulls city and state
+    # Pulls state
     state <- place_statefp[i]
     
+    # Pulls population data for tracts in 2019 for state with GEOID and population
     acs_populations <- tidycensus::get_acs(geography = "tract", variables = "B01003_001", year = 2019, state = state, geometry = FALSE) %>%
       select(GEOID, estimate)
     
+    # Creates dataframe with place, state, and total population estimate of place
     place_population <- places_tracts %>% 
       st_drop_geometry() %>%
       filter(STATEFP == state) %>%
@@ -207,15 +239,19 @@ if (!file.exists("outputs/places_pop_gt_50k.RDS")) {
       group_by(STATEFP, NAME) %>%
       summarise(estimate = sum(estimate))
     
+    # Coerces data to dataframe with column names
     temp_df <- data.frame(place_population)
     colnames(temp_df) <- c("STATEFP.PLACE", "PLACE", "POPULATION")
     
+    # Adds data to total dataframe
     population_table <- rbind(population_table, temp_df)
   }
   
+  # Filters to places with a population greater than 50k, adds geometry
   places_pop_gt_50k <- population_table %>%
     filter(POPULATION >= 50000) %>%
     left_join(places, by = c("STATEFP.PLACE" = "STATEFP", "PLACE" = "NAME"))
   
+  # Saves to outputs
   saveRDS(places_pop_gt_50k, "outputs/places_pop_gt_50k.RDS")
 }
